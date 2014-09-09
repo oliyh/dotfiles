ea4cd89a179eed87e5b6f080ebe6dae733605541
Oliver Hine <git@oliy.co.uk>
2014-09-09 12:54:32 +0000

    cider 0.7.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,9 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
                   [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]
+                  [lein-cljsbuild "1.0.3"]
+                  [jonase/eastwood "0.1.3"]
+                  [lein-cloverage "1.0.2"]
                   [lein-try "0.4.3"]]
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.nrepl "0.2.10"]]}}

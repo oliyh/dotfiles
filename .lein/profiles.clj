42f3f97c501da9a7ad2e337acff975656fa4e479
Oliver Hine <git@oliy.co.uk>
2016-02-04 19:15:32 +0000

    cljs repl works

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.1"]
+{:user {:plugins [[cider/cider-nrepl "0.10.2"]
                   ;;[refactor-nrepl "1.1.0"]
                   [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]
                   [jonase/eastwood "0.1.3"]
